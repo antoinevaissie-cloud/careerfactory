a32712d220f46e9b7397ad3df7c55444bb35303c
Antoine Vaissie <hello@antoinev.com>
2023-02-11 14:55:56 +0100

    campaign new presque finie
    Signed-off-by: Antoine Vaissie <hello@antoinev.com>

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -5,9 +5,22 @@ class CampaignsController < ApplicationController
   def new
     @recruiters = User.where(role: "Recruiter")
     @campaign = Campaign.new
+    @start_dates = []
+    @end_dates = []
+
+    start_time = Time.new(2023, 3, 20, 9, 0, 0)
+    end_time = Time.new(2023, 3, 24, 18, 0, 0)
+
+    while start_time <= end_time
+      @start_dates << start_time
+      @end_dates << start_time + 1.hour
+
+      start_time += 1.hour
+    end
   end
 
   def create
+    raise
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name_and_company
+    "#{first_name} #{last_name} : #{company}"
+  end
+
 end
