8bf4565d64f8aa8883929c5f32dd980db3445e0f
florence martino <florence.martino@lifen.fr>
2023-02-24 13:53:08 +0100

    refacto current_campaign

--- app/controllers/recruiters_controller.rb
+++ app/controllers/recruiters_controller.rb
@@ -2,9 +2,10 @@ class RecruitersController < ApplicationController
 
   def index
     if params[:campaign_id]
-      @recruiters = Campaign.find(params[:campaign_id]).users
+      current_campaign = Campaign.find(params[:campaign_id])
+      @recruiters = current_campaign.users
 
-      @slot_size_in_min = "#{Campaign.find(params[:campaign_id]).slot_size}min"
+      @slot_size_in_min = "#{current_campaign.slot_size}min"
 
     else
       @recruiters = User.where(role: "Recruiter")
