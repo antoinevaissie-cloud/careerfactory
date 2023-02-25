eefa163127c77e17fb29db306b0c3dac36aa904d
francois delattre <francoisdel@gmail.com>
2023-02-25 16:14:28 +0100

    un menu qui change en fonction de l utilisateur
    Signed-off-by: francois delattre <francoisdel@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/menu', to: "pages#menu", as: "menu"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
